f09a94e656ca4d0727c9045b4826ccf995ed242e
BenjaminNamayandev <benjaminnamayandeh@gmail.com>
2025-02-06 15:31:57 -0500

    Add a side navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 
 const navbar = () => {
+    const sideMenuRef = useRef();
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
+
     return (
         <div>
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -21,9 +33,29 @@ const navbar = () => {
                     <li><a href="#contact">Contact Me</a></li>
                 </ul>
 
-                <div>
+                <div className='flex items-center gap-4'>
+                    <button>
+                        <Image src={assets.moon_icon} alt='' className='w-5' />
+                    </button>
                     <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact<Image src={assets.arrow_icon} alt="" className='w-3' /></a>
+
+                    <button className='block md:hidden ml-3' onClick={openMenu}>
+                        <Image src={assets.menu_black} alt='' className='w-6' />
+                    </button>
                 </div>
+
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+                    <div className='absolute right-6 top-6' onClick={closeMenu}>
+                        <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+                    </div>
+
+                    <li><a onClick={closeMenu} href="#top">Home</a></li>
+                    <li><a onClick={closeMenu} href="#about">About me</a></li>
+                    <li><a onClick={closeMenu} href="#skills">Skills</a></li>
+                    <li><a onClick={closeMenu} href="#projects">Projects</a></li>
+                    <li><a onClick={closeMenu} href="#contact">Contact Me</a></li>
+                </ul>
             </nav>
         </div>
     )
